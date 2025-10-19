9584c1e89fb37c6ad6b3735296f78f50e428f3e1
Juno <juno.paakkanen@gmail.com>
2025-10-19 04:27:46 +0300

    Initial project setup

---
+++ src/main/java/com/juno/event_data_dashboard/EventDataDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.juno.event_data_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDataDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDataDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juno/event_data_dashboard/EventDataDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.event_data_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDataDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
